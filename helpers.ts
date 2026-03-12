41f251c0f3eabc232d73d6263d9038decd516585
frkahmtyyl <frkahmtyyl@gmail.com>
2026-03-12 06:38:30 +0300

    Patch API endpoint error handling

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T03:36:42.197Z
-Commit: Fix memory leak in event handlers
+Timestamp: 2026-03-12T03:38:30.176Z
+Commit: Patch API endpoint error handling
