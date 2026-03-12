dfd0db3f35a6f6e68c66ae23ee2554eb08905e07
frkahmtyyl <frkahmtyyl@gmail.com>
2026-03-12 06:39:24 +0300

    Patch security vulnerability in authentication

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-03-12T03:38:31.389Z
-Commit: Add multi-language support
+Timestamp: 2026-03-12T03:39:24.386Z
+Commit: Patch security vulnerability in authentication
