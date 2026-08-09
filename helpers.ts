b93076cd1a6e8437e9f9026bb46bbab50ec95415
frkahmtyyl <frkahmtyyl@gmail.com>
2026-08-09 03:22:10 +0300

    Resolve null pointer exception in user service

--- helpers.ts
+++ helpers.ts
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-08-09T00:21:10.586Z
-Commit: Correct typo in configuration file
+Timestamp: 2026-08-09T00:22:10.328Z
+Commit: Resolve null pointer exception in user service
