278b67f79374403d4b5fa00f0b3f7d0236e01efb
frkahmtyyl <frkahmtyyl@gmail.com>
2026-08-11 03:46:21 +0300

    Resolve null pointer exception in user service

--- main.py
+++ main.py
@@ -1,3 +1,3 @@
 Guild.xyz verification
-Timestamp: 2026-08-11T00:46:20.356Z
-Commit: Add dark mode support
+Timestamp: 2026-08-11T00:46:21.905Z
+Commit: Resolve null pointer exception in user service
